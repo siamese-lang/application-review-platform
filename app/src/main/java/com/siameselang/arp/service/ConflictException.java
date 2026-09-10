04a158a5ce0fa978f5a96b278a481470c15e79c8
siamese-lang <askj95@naver.com>
2026-09-11 03:01:35 +0900

    feat: add M5 business services

---
+++ app/src/main/java/com/siameselang/arp/service/ConflictException.java
@@ -0,0 +1,7 @@
+package com.siameselang.arp.service;
+
+public class ConflictException extends BusinessRuleException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
