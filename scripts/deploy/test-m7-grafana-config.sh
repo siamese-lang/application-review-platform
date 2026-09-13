6d97475331637fe96c5454a965cfdb13d960b6b2
siamese-lang <askj95@naver.com>
2026-09-14 03:23:34 +0900

    Assert live blackbox dashboard label

--- scripts/deploy/test-m7-grafana-config.sh
+++ scripts/deploy/test-m7-grafana-config.sh
@@ -47,6 +47,7 @@ for dashboard in \
 done
 
 grep -Fq '"uid": "arp-system-overview"' "$dashboard_dir/system-overview.json"
+grep -Fq 'probe_success{job=\\"integrations/blackbox/application-api\\",node=\\"obs-01\\",role=\\"observability\\"}' "$dashboard_dir/system-overview.json"
 grep -Fq '"uid": "arp-application-api"' "$dashboard_dir/application-api.json"
 grep -Fq '"uid": "arp-postgresql"' "$dashboard_dir/postgresql.json"
 grep -Fq '"uid": "arp-garage"' "$dashboard_dir/garage.json"
